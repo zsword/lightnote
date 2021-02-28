038e240de3f4999edf4d9456f8671a16037fde48
JemiChow <jemichow@163.com>
2021-02-28 22:09:16 +0800

    Update run script

--- bin/run.bat
+++ bin/run.bat
@@ -1,23 +1,17 @@
 @echo off
+title LeaNote
 
-cd..
+cd %~dp0
+cd ..
 set SCRIPTPATH=%cd%
-
+cd bin
 : top src directory
 set leanotePath="%SCRIPTPATH%\bin\src\github.com\leanote"
-
-if not exist "%leanotePath%" mkdir "%leanotePath%"
 
 : create software link
 if exist "%leanotePath%\leanote" del /Q "%leanotePath%\leanote"
 mklink /D "%leanotePath%\leanote"  %SCRIPTPATH%
 
-: set GOPATH
-set GOPATH="%SCRIPTPATH%\bin"
-
-: run
-if %processor_architecture%==x86 (
-	"%SCRIPTPATH%\bin\leanote-windows-386.exe" -importPath github.com/leanote/leanote
-) else (
-	"%SCRIPTPATH%\bin\leanote-windows-amd64.exe" -importPath github.com/leanote/leanote
-)
+call lightnote.exe -importPath github.com/leanote/leanote -srcPath "./src" -runMode prod
+cd %~dp0
+pause
