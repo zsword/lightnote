61efa48c06b08495234e1fe04b701db41bc85611
JemiChow <jemichow@163.com>
2021-03-12 09:48:49 +0800

    Update build script

--- bin/buildApp.bat
+++ bin/buildApp.bat
@@ -2,9 +2,15 @@
 title Build LightNote
 
 cd %~dp0
-cd ..
-call revel build /m prod /a ./
+cd ..\
+rmdir /S/Q target
+call revel build /m prod /a .
 call gulp
 xcopy messages\ target\messages\ /E /Y /Q
+cd target
+rmdir /S/Q app
+for /d %%f in (src\github.com\leanote\leanote\*) do (
+  move /Y %%f .\
+)
 cd %~dp0
 pause
